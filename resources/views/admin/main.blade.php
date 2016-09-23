ba4185d4d5a4ea4d9cb6456e094c103fc4dc41ab
edwin <lark930@gmail.com>
2016-09-23 15:39:32 +0800

    Change Admin name

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>KYC</title>
+    <title>TGIF Admin</title>
     {{--        <link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
     <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
